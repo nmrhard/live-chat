873ae74d3eb522453be9ec284f9bc205cb7b4a21
nmr <nmrhard@gmail.com>
2021-06-12 22:29:42 +0300

    Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <div>
+      <p>Hey there ... display name</p>
+      <p class="email">Currently logged in as ... email</p>
+    </div>
+    <button type="button">Logout</button>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
