42aa53109488ff17e8c55bd6b0c2db2121014d37
nmr <nmrhard@gmail.com>
2021-06-12 19:39:22 +0300

    Creating a Signup and Login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="email" placeholder="Email" required v-model="email">
+    <input type="password" placeholder="Password" required v-model="password">
+    <button type="submit">Login in</button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+export default {
+  setup() {
+    const email = ref('');
+    const password = ref('');
+
+    const handleSubmit = function () {
+      
+    }
+
+    return { email, password, handleSubmit }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" placeholder="Display name" required v-model="displayName">
+    <input type="email" placeholder="Email" required v-model="email">
+    <input type="password" placeholder="Password" required v-model="password">
+    <button type="submit">Sign up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+export default {
+  setup() {
+    const displayName = ref('');
+    const email = ref('');
+    const password = ref('');
+
+    const handleSubmit = function () {
+      
+    }
+
+    return { displayName, email, password, handleSubmit }
+  }
+}
+</script>
+
+<style>
+
+</style>
