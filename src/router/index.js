8856eea63bef6a3e0f7e1f219ab35f9caf1d295a
nmr <nmrhard@gmail.com>
2021-06-13 15:21:34 +0300

    Route guards(for unauth users)

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from '../views/Welcome.vue'
 import Chatroom from '../views/Chatroom.vue'
+import { projectAuth } from '../firebase/config.js';
+
+// auth guard
+const requireAuth = function(to, from, next) {
+  let user = projectAuth.currentUser;
+
+  if(!user) {
+    next({ name: 'Welcome' });
+  } else {
+    next();
+  }
+} 
 
 const routes = [
   {
@@ -11,7 +23,8 @@ const routes = [
   {
     path: '/chatroom',
     name: 'Chatroom',
-    component: Chatroom
+    component: Chatroom,
+    beforeEnter: requireAuth
   },
 ]
 
